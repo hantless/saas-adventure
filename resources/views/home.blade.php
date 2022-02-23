304f0dde3a01f67f82da6e863560959466bbb53d
Frédéric Arijs <frederic.arijs@ext.bewan.be>
2022-02-23 17:37:33 +0100

    Blade main template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('app')
 
-  <link rel="apple-touch-icon" sizes="57x57" href="/img/favicons/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/img/favicons/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/img/favicons/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/img/favicons/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/img/favicons/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/img/favicons/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/img/favicons/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/img/favicons/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="/img/favicons/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/img/favicons/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
-  <link rel="manifest" href="/img/favicons/manifest.json">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="/img/favicons/ms-icon-144x144.png">
-  <meta name="theme-color" content="#ffffff">
+@section('title', 'WeTeach - A Course Platform for Developers, built by Developers')
 
-  <link href="/css/app.css" rel="stylesheet">
-  <title>WeTeach - A Course Platform for Developers, built by Developers</title>
-  <style>
-    .gradient {
-        background: linear-gradient(45deg, #667eea, #ed64a6);
-    }
-  </style>
-</head>
-<body class="antialiased">
-
+@section('content')
   <!-- top color bar -->
   <div class="h-2 bg-indigo-600 w-full absolute z-20 gradient"></div>
 
@@ -199,5 +169,4 @@
       }
   </style>
 
-</body>
-</html>
+@endsection
